eba96e0751807028e5d39a5294bfa86687a7da32
osakanaa13 <77139372+osakanaa13@users.noreply.github.com>
2021-01-09 14:29:19 +0800

    first

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'Nzk3MDU5NzYyMzc3NTg4Nzg2.X_g9wA.ctxoNEvyjWO1Zr1lLBxlHvdguVs',
+        prefix: '/',
+        activity: 'nakipag Suntuccan',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
